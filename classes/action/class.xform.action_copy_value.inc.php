b2118f33a224651c546aba4bd8f9da46c701d6d1
aeberhard <andreaseberhard@gmail.com>
2011-07-07 00:11:10 +0200

    code review

--- classes/action/class.xform.action_copy_value.inc.php
+++ classes/action/class.xform.action_copy_value.inc.php
@@ -1,32 +1,40 @@
 <?php
 
+/**
+ * XForm
+ *
+ * @author jan.kristinus[at]redaxo[dot]de Jan Kristinus
+ * @author <a href="http://www.yakamara.de">www.yakamara.de</a>
+ *
+ * @package redaxo4
+ * @version svn:$Id$
+ */
+
 class rex_xform_action_copy_value extends rex_xform_action_abstract
 {
 
-	function execute()
-	{
-	
-		$label_from = $this->action["elements"][2];
-		$label_to = $this->action["elements"][3];
-	
-		foreach($this->elements_sql as $key => $value)
-		{
-			if ($label_from==$key)
-			{
-				$this->elements_sql[$label_to] = $value;
-				break;
-			}
-		}
-		
-		return;
+  function execute()
+  {
+  
+    $label_from = $this->getElement(2);
+    $label_to = $this->getElement(3);
+  
+    foreach($this->elements_sql as $key => $value)
+    {
+      if ($label_from==$key)
+      {
+        $this->elements_sql[$label_to] = $value;
+        break;
+      }
+    }
+    
+    return;
 
-	}
+  }
 
-	function getDescription()
-	{
-		return "action|copy_value|label_from|label_to";
-	}
+  function getDescription()
+  {
+    return "action|copy_value|label_from|label_to";
+  }
 
 }
-
-?>
