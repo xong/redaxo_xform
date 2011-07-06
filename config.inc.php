98a0b89d0ce5b2d36422bb78e9f0d0d88857494b
jdlx <github@rexdev.de>
2011-07-06 16:41:31 +0200

    restore subpage name..

--- config.inc.php
+++ config.inc.php
@@ -30,7 +30,7 @@ if($REX['REDAXO'] && $REX['USER'])
 	$REX['ADDON'][$mypage]['SUBPAGES'] = array();
 	//$REX['ADDON'][$mypage]['SUBPAGES'][] = array( '' , $I18N->msg("xform_overview"));
 	if ($REX['USER']->isAdmin() || $REX['USER']->hasPerm("xform[]")) 
-		$REX['ADDON'][$mypage]['SUBPAGES'][] = array ('' , $I18N->msg("xform_description"));
+		$REX['ADDON'][$mypage]['SUBPAGES'][] = array ('description' , $I18N->msg("xform_description"));
 	
 	function rex_xform_css($params){
 		global $REX;
