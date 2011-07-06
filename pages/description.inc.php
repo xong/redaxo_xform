8b25075c61aee501e492ed135178cd93bd79cdce
jdlx <github@rexdev.de>
2011-07-06 02:27:40 +0200

    reformat description..

--- pages/description.inc.php
+++ pages/description.inc.php
@@ -5,10 +5,9 @@ rex_title("XForm", $REX['ADDON'][$page]['SUBPAGES']);
 echo '<div class="rex-addon-output">
 	<h2 class="rex-hl2">Beschreibung</h2>
 	<div class="rex-addon-content">
-	<p>'.$I18N->msg('xform_description_all').'</p>
-	<hr />';
+	<div class="xform-description">'.$I18N->msg('xform_description_all').'</div>';
 
-rex_xform::showHelp(); 
+echo rex_xform::showHelp(true,true);
 
 echo '
 	</div>
