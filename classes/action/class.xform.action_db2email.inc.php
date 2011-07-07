687f6c9a9bdd8f4f68b859ca1e296586527598f2
aeberhard <andreaseberhard@gmail.com>
2011-07-07 23:09:49 +0200

    code review, debugmeldungen bei debug aktiv

--- classes/action/class.xform.action_db2email.inc.php
+++ classes/action/class.xform.action_db2email.inc.php
@@ -1,76 +1,89 @@
 <?php
 
+/**
+ * XForm
+ *
+ * @author jan.kristinus[at]redaxo[dot]de Jan Kristinus
+ * @author <a href="http://www.yakamara.de">www.yakamara.de</a>
+ *
+ * @package redaxo4
+ * @version svn:$Id$
+ */
+
 class rex_xform_action_db2email extends rex_xform_action_abstract
 {
-	
-	function execute()
-	{
+  
+  function execute()
+  {
 
-		global $REX;
+    global $REX;
 
-		$template_name = $this->action["elements"][2];
+    $template_name = $this->getElement(2);
 
-		if($etpl = rex_xform_emailtemplate::getTemplate($template_name))
-		{
+    if($etpl = rex_xform_emailtemplate::getTemplate($template_name))
+    {
 
-			// ----- find mailto
-			$mail_to = $REX['ERROR_EMAIL']; // default
-			
-			// finde email label in list
-			if (isset($this->action["elements"][3]) && $this->action["elements"][3] != "")
-			{
-				foreach($this->elements_email as $key => $value)
-					if ($this->action["elements"][3]==$key)
-					{
-						$mail_to = $value;
-						break;
-					}
-			}
-			
-			// ---- fix mailto from definition
-			if (isset($this->action["elements"][4]) && $this->action["elements"][4] != "") 
-				$mail_to = $this->action["elements"][4];
-		
-			$etpl = rex_xform_emailtemplate::replaceVars($etpl,$this->elements_email);
-		
-			$etpl['mail_to'] = $mail_to;
-			$etpl['mail_to_name'] = $mail_to;
-			
-			if($etpl['attachments'] != "")
-			{
-				$f = explode(",",$etpl['attachments']);
-				$etpl['attachments'] = array();
-				foreach($f as $v)
-				{
-					$etpl['attachments'][] = array("name"=>$v,"path"=>$REX["INCLUDE_PATH"].'/../../files/'.$v);
-				}
-				
-			}else
-			{
-				$etpl['attachments'] = array();
-			}
-			
-			if(!rex_xform_emailtemplate::sendMail($etpl))
-			{
-				echo "Fehler beim E-Mail Versand";
-				return FALSE;
-			}
+      // ----- find mailto
+      $mail_to = $REX['ERROR_EMAIL']; // default
 
-			return TRUE;
-		
-		}
+      // finde email label in list
+      if ($this->getElement(3) != FALSE && $this->getElement(3) != "")
+      {
+        foreach($this->elements_email as $key => $value)
+          if ($this->action["elements"][3]==$key)
+          {
+            $mail_to = $value;
+            break;
+          }
+      }
+      
+      // ---- fix mailto from definition
+      if ($this->getElement(4) != FALSE && $this->getElement(4) != "")
+        $mail_to = $this->getElement(4);
+    
+      $etpl = rex_xform_emailtemplate::replaceVars($etpl,$this->elements_email);
+    
+      $etpl['mail_to'] = $mail_to;
+      $etpl['mail_to_name'] = $mail_to;
+      
+      if($etpl['attachments'] != "")
+      {
+        $f = explode(",",$etpl['attachments']);
+        $etpl['attachments'] = array();
+        foreach($f as $v)
+        {
+          $etpl['attachments'][] = array("name"=>$v,"path"=>$REX["INCLUDE_PATH"].'/../../files/'.$v);
+        }
+        
+      }else
+      {
+        $etpl['attachments'] = array();
+      }
+      
+      if ($this->params["debug"])
+      {			
+        echo "<hr /><pre>"; var_dump($etpl); echo "</pre><hr />"; 
+      }
+      
+      if(!rex_xform_emailtemplate::sendMail($etpl))
+      {
+        echo "Fehler beim E-Mail Versand";
+        return FALSE;
+      }
 
-		return FALSE;
+      return TRUE;
+    
+    }
 
-	}
+    return FALSE;
 
-	function getDescription()
-	{
+  }
 
-		return "action|db2email|namekey|emaillabel|[email@domain.de]";
+  function getDescription()
+  {
 
-	}
+    return "action|db2email|emailtemplate|emaillabel|[email@domain.de]";
+
+  }
 
 }
-
-?>
