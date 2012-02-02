7499fd85d50e2e37bf47f3dc5f382bacdfaa79a5
Markus Lorch <m.lorch@it-kult.de>
2012-02-02 12:23:44 +0100

    fixing layout for r5

--- pages/index.inc.php
+++ pages/index.inc.php
@@ -5,7 +5,7 @@
 
 $t = new rex_xform_manager();
 $tables = $t->getTables();
-
+$content ='';
 
 $page = 'xform';
 
@@ -13,33 +13,31 @@ $subpage = rex_request("subpage","string");
 
 if ($subpage != "")
 {
-	switch($subpage)
-	{
-		case('description'):
-			include rex_path::addon("xform","pages/description.inc.php");
-			break;
-		default:
-			include rex_path::plugin("xform",$subpage,"pages/index.inc.php");
-			break;
-	}
+  switch($subpage)
+  {
+    case('description'):
+      include rex_path::addon("xform","pages/description.inc.php");
+      break;
+    default:
+      include rex_path::plugin("xform",$subpage,"pages/index.inc.php");
+    break;
+  }
 
-}else
+}
+else
 {
+  $content .= '<h2>XFORM - '.rex_i18n::msg("xform_overview").'</h2>';
 
-	echo rex_view::title(rex_i18n::msg('xform'),rex_addon::get('xform')->getProperty('subpages'));
+  /*echo '<ul>';
+  foreach($REX['ADDON'][$page]['SUBPAGES'] as $sp)
+  {
+  echo '<li><a href="index.php?page='.$page.'&amp;subpage='.$sp[0].'">'.$sp[1].'</a></li>';
+  }  
+  echo '</ul>';*/
 
-	echo '<div class="rex-addon-output">';
-	echo '<h2 class="rex-hl2">XFORM - '.rex_i18n::msg("xform_overview").'</h2>';
-	
-	echo '<div class="rex-addon-content"><ul>';
-	/*
-	foreach($REX['ADDON'][$page]['SUBPAGES'] as $sp)
-	{
-		echo '<li><a href="index.php?page='.$page.'&amp;subpage='.$sp[0].'">'.$sp[1].'</a></li>';
-	}
-	*/
-	echo '</ul></div>';
-	echo '</div>';
+  ## output
+  echo rex_view::title(rex_i18n::msg('xform'),rex_addon::get('xform')->getProperty('subpages'));
+  echo rex_view::contentBlock($content,'','tab');
 }
 
 ?>
