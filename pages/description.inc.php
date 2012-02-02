0e58081ed7e341618fc26dd2454b4882ae84bc7b
Markus Lorch <m.lorch@it-kult.de>
2012-02-02 01:15:24 +0100

    fixing style (2)

--- pages/description.inc.php
+++ pages/description.inc.php
@@ -1,9 +1,8 @@
 <?php
-
 echo rex_view::title("XForm");
 
 $content = '<h2>'.rex_i18n::msg('xform_description').'</h2>';
-$content .= '<div class="xform-description">'.rex_i18n::msg('xform_description_all').'</div>';
+$content .= rex_i18n::msg('xform_description_all');
 $content .= rex_xform::showHelp(true,true);
 
 echo rex_view::contentBlock($content,'','tab');
